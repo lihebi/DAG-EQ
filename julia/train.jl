c26250b808b01b297aaf4c87083754bc7c85e92b
Hebi Li <lihebi.com@gmail.com>
2019-12-10 16:15:20 -0600

    more metrics; use same noise to train g and d

--- julia/train.jl
+++ julia/train.jl
@@ -1,5 +1,9 @@
 using ProgressMeter
 using CuArrays: allowscalar
+using Flux: @epochs, onecold
+# mean
+using Statistics
+
 
 allowscalar(false)
 # allowscalar(true)
@@ -26,7 +30,9 @@ function reset!(m::MeanMetric)
     m.n = 0
 end
 
+my_accuracy(ŷ, y) = mean(onecold(cpu(ŷ)) .== onecold(cpu(y)))
 
+# https://github.com/soumith/ganhacks
 function train_GAN!(g, d, gopt, dopt, ds;
                     train_steps=ds.nbatch)
     ps_g=Flux.params(g)
@@ -34,6 +40,9 @@ function train_GAN!(g, d, gopt, dopt, ds;
 
     loss_g_metric = MeanMetric()
     loss_d_metric = MeanMetric()
+    dx_metric = MeanMetric()
+    dgz1_metric = MeanMetric()
+    dgz2_metric = MeanMetric()
 
     @showprogress 0.1 "Training..." for step in 1:train_steps
         x, y = next_batch!(ds)
@@ -41,47 +50,65 @@ function train_GAN!(g, d, gopt, dopt, ds;
         y = gpu(y)
 
         # for calculating real/fake labels
-        real_out = d(x)
+        out = d(x)
         noise = gpu(Float32.(randn(100, size(x)[end])))
-        real_label = gpu(Float32.(ones(size(real_out))))
-        fake_label = gpu(Float32.(zeros(size(real_out))))
+        real_label = gpu(Float32.(ones(size(out))))
+        fake_label = gpu(Float32.(zeros(size(out))))
 
         # update D
         gs = gradient(ps_d) do
             real_out = d(x)
             l1 = bce(real_out, real_label)
+
+            add!(dx_metric, mean(σ.(real_out)))
+
             fake = g(noise)
             fake_out = d(fake)
             l2 = bce(fake_out, fake_label)
 
+            add!(dgz1_metric, mean(σ.(fake_out)))
+
             loss = l1 + l2
             add!(loss_d_metric, loss)
+
             loss
         end
         Flux.Optimise.update!(dopt, ps_d, gs)
 
         # update G
-        # FIXME using a new copy of noise
-        noise = gpu(Float32.(randn(100, size(x)[end])))
+        #
+        # FIXME NOT using a new copy of noise
+        # noise = gpu(Float32.(randn(100, size(x)[end])))
         gs = gradient(ps_g) do
+            # FIXME I have to regenerate the fake, otherwise the gradient
+            # calculation by Zygote will not consider it?
             fake = g(noise)
             fake_out = d(fake)
             loss = bce(fake_out, real_label)
+
+            add!(dgz2_metric, mean(σ.(fake_out)))
             add!(loss_g_metric, loss)
+
             loss
         end
         Flux.Optimise.update!(gopt, ps_g, gs)
 
         # loss_D, loss_G, D(x), D(G(z))
-        if step % 100 == 0
+        if step % 200 == 0
             println()
-            loss_g = get!(loss_g_metric)
-            loss_d = get!(loss_d_metric)
-            @info "data" loss_g loss_d
+            @info("data",
+                  get!(loss_g_metric),
+                  get!(loss_d_metric),
+                  get!(dx_metric),
+                  get!(dgz1_metric),
+                  get!(dgz2_metric))
             # also print out a sample
-            noise = randn(100, 32) |> gpu;
+            # noise = randn(100, 32) |> gpu;
             fake = g(noise);
             sample_and_view(fake)
+            # I want to print out the real and fake accuracy by D
+            @show my_accuracy(d(x), real_label)
+            @show my_accuracy(d(g(noise)), fake_label)
         end
     end
 end
