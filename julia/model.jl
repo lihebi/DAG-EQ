903eb3fbf0693a40540b27babb51e5d41f8ef196
Hebi Li <lihebi.com@gmail.com>
2019-12-10 16:14:50 -0600

    add more GAN models

--- julia/model.jl
+++ julia/model.jl
@@ -27,41 +27,6 @@ function (l::Tanh)(input)
     tanh.(input)
 end
 
-using Flux: expand
-
-struct SamePad end
-
-# from https://github.com/FluxML/Flux.jl/pull/901
-calc_padding(pad, k::NTuple{N,T}, dilation, stride) where {T,N}= expand(Val(2*N), pad)
-function calc_padding(::SamePad, k::NTuple{N,T}, dilation, stride) where {N,T}
-    #Formula from Relationship 14 in
-    #http://deeplearning.net/software/theano_versions/dev/tutorial/conv_arithmetic.html
-
-    # Effective kernel size, including dilation
-    k_eff = @. k + (k - 1) * (dilation - 1)
-    # How much total padding needs to be applied?
-    pad_amt = @. k_eff - 1
-    # In case amount of padding is odd we need to apply different amounts to each side.
-    return Tuple(mapfoldl(i -> [ceil(Int, i/2), i ÷ 2], vcat, pad_amt))
-end
-
-function Conv(w::AbstractArray{T,N}, b::AbstractVector{T}, σ = identity;
-              stride = 1, pad = 0, dilation = 1) where {T,N}
-  stride = expand(Val(N-2), stride)
-  # pad = expand(Val(2*(N-2)), pad)
-  dilation = expand(Val(N-2), dilation)
-  pad = calc_padding(pad, size(w)[1:N-2], dilation, stride)
-  return Conv(σ, w, b, stride, pad, dilation)
-end
-
-function ConvTranspose(w::AbstractArray{T,N}, b::AbstractVector{T}, σ = identity;
-              stride = 1, pad = 0, dilation = 1) where {T,N}
-  stride = expand(Val(N-2), stride)
-  # pad = expand(Val(2*(N-2)), pad)
-  dilation = expand(Val(N-2), dilation)
-  pad = calc_padding(pad, size(w)[1:N-2], dilation, stride)
-  return ConvTranspose(σ, w, b, stride, pad, dilation)
-end
 
 function generator()
     Chain(ConvTranspose((4,4), 100=>512, stride=1, pad=1),
@@ -96,31 +61,128 @@ function discriminator()
           Conv((4,4), 512=>1, stride=1, pad=1))
 end
 
+# FIXME Zygote currently does not differentiate this code, neither fill! nor
+# ones https://github.com/FluxML/Zygote.jl/issues/150
+function upsample(x)
+    ratio = (2, 2, 1, 1)
+    (h, w, c, n) = size(x)
+    y = similar(x, (1, ratio[1], 1, ratio[2], 1, 1))
+    # FIXME fill! does not work?
+    # fill!(y, 1)
+    y2 = ones(size(y))
+    z = reshape(x, (h, 1, w, 1, c, n))  .* y2
+    reshape(permutedims(z, (2,1,4,3,5,6)), size(x) .* ratio)
+end
+
+# BROKEN due to upsample undifferentiable
+function generator_dcgan()
+    Chain(Dense(100, 128 * 7 * 7),
+          x -> reshape(x, 7, 7, 128, :),
+          BatchNorm(128),
+          upsample,
+          Conv((3,3), 128=>128, stride=1, pad=1),
+          BatchNorm(128),
+          LeakyReLU(),
+          upsample,
+          Conv((3,3), 128=>64, stride=1, pad=1),
+          BatchNorm(64),
+          LeakyReLU(),
+          Conv((3,3), 64=>1, stride=1, pad=1),
+          Tanh())
+end
+
+function generator_dcgan_ct()
+    Chain(Dense(100, 128 * 7 * 7),
+          x -> reshape(x, 7, 7, 128, :),
+          BatchNorm(128),
+          # since upsampling does not work, I'm still going to use ConvT
+          ConvTranspose((4,4), 128=>128, stride=2, pad=1),
+          BatchNorm(128),
+          LeakyReLU(),
+          ConvTranspose((4,4), 128=>64, stride=2, pad=1),
+          BatchNorm(64),
+          LeakyReLU(),
+          ConvTranspose((3,3), 64=>1, stride=1, pad=1),
+          Tanh())
+end
+
+# from https://github.com/eriklindernoren/PyTorch-GAN dcgan/
+function discriminator_dcgan()
+    Chain(Conv((3,3), 1=>16, stride=2, pad=1),
+          LeakyReLU(),
+          Dropout(0.25),
+          # NOTE: no dropout for the first conv
+          Conv((3,3), 16=>32, stride=2, pad=1),
+          LeakyReLU(),
+          # CAUTION dropout turns out to be very important
+          # NOTE: using both dropout and batchnorm
+          Dropout(0.25),
+          BatchNorm(32),
+          Conv((3,3), 32=>64, stride=2, pad=1),
+          LeakyReLU(),
+          Dropout(0.25),
+          BatchNorm(64),
+          Conv((3,3), 64=>128, stride=2, pad=1),
+          LeakyReLU(),
+          Dropout(0.25),
+          BatchNorm(128),
+          Flatten(),
+          Dense(128 * 2 * 2, 1))
+end
+
+
+# https://github.com/FluxML/model-zoo/pull/111
+function generator_111()
+    generator = Chain(
+        Dense(100, 1024, leakyrelu),
+        BatchNorm(1024),
+        Dense(1024, 7 * 7 * 128, leakyrelu),
+        BatchNorm(7 * 7 * 128),
+        x->reshape(x, 7, 7, 128,:),
+        ConvTranspose((4,4), 128=>64, relu; stride=(2,2), pad=(1,1)),
+        BatchNorm(64),
+        ConvTranspose((4,4), 64=>1, tanh; stride=(2,2), pad=(1,1)))
+end
+
+function discriminator_111()
+    discriminator = Chain(
+        Conv((3,3), 1=>32, leakyrelu;pad = 1),
+        MaxPool((2,2)),
+        Conv((3,3), 32=>64, leakyrelu;pad = 1),
+        MaxPool((2,2)),
+        x->reshape(x,7*7*64,:),
+        Dense(7*7*64, 1024, leakyrelu),
+        BatchNorm(1024),
+        Dense(1024, 1))
+end
+
 # from https://www.tensorflow.org/tutorials/generative/dcgan
 function generator_MNIST()
     Chain(Dense(100, 7*7*256),
           x -> reshape(x, 7, 7, 256, :),
           BatchNorm(256),
           LeakyReLU(),
-          ConvTranspose((5,5), 256=>128, stride=1, pad=SamePad()),
+          ConvTranspose((4,4), 256=>128, stride=2, pad=1),
           BatchNorm(128),
           LeakyReLU(),
           # FIXME this is not 14,14,64, but 13,13,64
-          # ConvTranspose((5,5), 128=>64, stride=2, pad=SamePad()),
+          # ConvTranspose((5,5), 128=>64, stride=2, pad=2),
           ConvTranspose((4,4), 128=>64, stride=2, pad=1),
           BatchNorm(64),
           LeakyReLU(),
-          ConvTranspose((4,4), 64=>1, stride=2, pad=1),
+          ConvTranspose((3,3), 64=>1, stride=1, pad=1),
           Tanh())
 end
 
 function discriminator_MNIST()
-    Chain(Conv((5,5), 1=>64, stride=2, pad=SamePad()),
+    Chain(Conv((5,5), 1=>64, stride=2, pad=2),
           LeakyReLU(),
           Dropout(0.3),
-          Conv((5,5), 64=>128, stride=2, pad=SamePad()),
+          BatchNorm(64),
+          Conv((5,5), 64=>128, stride=2, pad=2),
           LeakyReLU(),
           Dropout(0.3),
+          BatchNorm(128),
           Flatten(),
           Dense(7*7*128, 1))
 end
--- julia/test.jl
+++ julia/test.jl
@@ -4,11 +4,6 @@ using LightGraphs
 
 using DelimitedFiles, LinearAlgebra
 
-# mean
-using Statistics
-
-using Flux: @epochs
-
 function test_CI()
 
     p = 0.01
@@ -29,9 +24,10 @@ function test_CI()
 
 end
 
-
+include("data.jl")
 include("model.jl")
 include("train.jl")
+
 
 function test()
     # create models
@@ -41,8 +37,46 @@ function test()
     g = generator_MNIST() |> gpu
     d = discriminator_MNIST() |> gpu
 
+    g = generator_111() |> gpu
+    d = discriminator_111() |> gpu
+
+    g = generator_dcgan() |> gpu
+    g = generator_dcgan_ct() |> gpu
+    d = discriminator_dcgan() |> gpu
+
     g = denseG() |> gpu
     d = denseD() |> gpu
+
+    # Loading MNIST data
+    ds, test_ds = load_MNIST_ds(batch_size=50)
+    x, y = next_batch!(ds);
+
+    # visualizing MNIST
+    size(x)
+    img = x[:,:,:,1];
+    MyImage(img)
+    sample_and_view(x)
+
+    # Training GAN
+    gopt = ADAM(2e-4)
+    dopt = ADAM(2e-4)
+    # dopt = Descent(1e-4)
+    @epochs 15 train_GAN!(g, d, gopt, dopt, ds)
+
+    # visualize the output
+    noise = randn(100, 32) |> gpu;
+    fake = g(noise);
+    sample_and_view(fake)
+
+end
+
+
+function test()
+    g = denseG() |> gpu
+    d = denseD() |> gpu
+
+    g = generator_dcgan_ct() |> gpu
+    d = discriminator_dcgan() |> gpu
 
     # DEBUG testing the model
     out = d(gpu(randn(28,28,1,32)));
@@ -73,28 +107,6 @@ function test()
     # d = discriminator_MNIST() |> gpu
     size(d(fake))
 
-
-    # Loading MNIST data
-    ds, test_ds = load_MNIST_ds(batch_size=50)
-    x, y = next_batch!(ds);
-
-    # visualizing MNIST
-    size(x)
-    img = x[:,:,:,1];
-    MyImage(img)
-    sample_and_view(x)
-
-
-    # Training GAN
-    gopt = ADAM(1e-4)
-    dopt = ADAM(1e-4)
-    @epochs 5 train_GAN!(g, d, gopt, dopt, ds)
-
-    # visualize the output
-    noise = randn(100, 32) |> gpu;
-    fake = g(noise);
-    sample_and_view(fake)
-
     # random testing
     gradient(x -> 3x^2 + 2x + 1, 5)
     gradient(x -> 3x^2 + 2x + eps(0.1), 5)
@@ -111,4 +123,3 @@ function test()
 
     gs[:layers]
 end
-
