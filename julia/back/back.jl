9e3a7eee573f25b9ce30563e4793f03c3fb4272e
Hebi Li <lihebi.com@gmail.com>
2019-12-13 10:41:23 -0600

    old supervised code

--- julia/back/back.jl
+++ julia/back/back.jl
@@ -41,3 +41,103 @@ function ensure_dag(g)
     # restore adj matrix
     DiGraph(m)
 end
+
+function sup_train!(model, opt, x, y, test_x, test_y)
+    # FIXME mse does not seem to fit for high dim
+    loss(x, y) = Flux.mse(model(x), y)
+
+    function cb_fn1()
+        @show Flux.mse(model(x), y)
+        # @show accuracy(model(x), y)
+        # @show accuracy(model(test_x), test_y)
+        @time metrics = sup_graph_metrics(model(x), y)
+        @show metrics
+    end
+
+    function cb_fn2()
+        @time test_metrics = sup_graph_metrics(model(test_x), test_y)
+        @info "test metrics $(test_metrics)"
+        # @show test_metrics
+    end
+
+    f1 = Flux.throttle(cb_fn1, 1)
+    # I want test metrics to be less frequent
+    f2 = Flux.throttle(cb_fn2, 10)
+
+    function cb_fn()
+        f1()
+        f2()
+    end
+
+    Flux.train!(loss, Flux.params(model), Iterators.repeated((x,y), 100), opt, cb=cb_fn)
+end
+
+function sup_model()
+    Chain(Dense(4, 100, relu),
+          Dense(100, 100, relu),
+          Dense(100, 4))
+end
+
+# NOTE sup exp should not use accuracy, but fpr, tpr
+accuracy(out, y) = mean(Flux.onecold(abs.(out)) .== Flux.onecold(abs.(y)))
+# tpr(out, y) = sum(reshape(y, 20, 20, :) .== adjacency_matrix(ĝ) .== 1)
+
+
+function test_sup_bi()
+    # Training
+    # 1. generate model g
+    g = MetaDiGraph(2)
+    add_edge!(g, 1, 2)
+
+    x, y = gen_sup_data(g)
+    test_x, test_y = gen_sup_data(g)
+
+    # generate data for the other direction
+    g2 = MetaDiGraph(2)
+    add_edge!(g2, 2, 1)
+
+    x2, y2 = gen_sup_data(g2)
+    test_x2, test_y2 = gen_sup_data(g2)
+
+
+    model = sup_model()
+    # opt = Descent(0.1)
+    opt = ADAM(1e-4)
+    Flux.@epochs 20 sup_train!(model, opt,
+                               hcat(x, x2), hcat(y, y2),
+                               hcat(test_x, test_x2), hcat(test_y, test_y2))
+    Flux.@epochs 5 sup_train!(model, opt, x2, y2)
+
+    accuracy(model(test_x), test_y)
+    accuracy(model(x2), y2)
+    accuracy(model(test_x2), test_y2)
+
+    # test whether it works
+    model(ds[1][:,1:10])
+    ds[2][:,1]
+    res = model(ds[1][:,1:10])
+    @show sum([x[1] == 3 for x in argmax(abs.(model(ds[1])), dims=1)]) / size(ds[1],2)
+    @show sum([x[1] == 3 for x in argmax(abs.(model(test_ds[1])), dims=1)]) / size(test_ds[1],2)
+    @show [x[1] for x in argmax(abs.(res), dims=1)]
+    res[CartesianIndex(4, 1)]
+    findmax
+    mapslices(indmax, model(ds[1][:,1:10]), 2)
+
+    # TODO generate new data and test
+
+    # 2. generate weights W
+    @info "Generating weights .."
+    W = gen_weights(g)
+    @show Matrix(W)
+    # 3. generate data X
+    @info "Generating 100 data points .."
+    X = gen_data(g, W, 100)
+    # 4. compute μ and σ (or just use the ground truth μ and σ)
+    μ = mean.(X)
+    σ = var.(X)
+    # 5. f(μ, σ) => W
+
+    # Inference
+    # 1. compute μ and σ
+    # 2. f(μ, σ)
+end
