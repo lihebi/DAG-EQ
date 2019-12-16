62f49bf6f9e53fa4fb928fc24f9c194268828a00
Hebi Li <lihebi.com@gmail.com>
2019-12-16 13:50:59 -0600

    fix data iterator dim

--- julia/src/data.jl
+++ julia/src/data.jl
@@ -23,16 +23,21 @@ function next_batch!(ds::DataSetIterator)
     if (ds.index + 1) * ds.batch_size > size(ds.raw_x)[end]
         ds.index = 0
     end
+
+    xdims = collect(repeated(:, length(size(ds.raw_x)) - 1))
+    ydims = collect(repeated(:, length(size(ds.raw_y)) - 1))
+
     if ds.index == 0
         indices = shuffle(1:size(ds.raw_x)[end])
-        ds.raw_x = ds.raw_x[size(ds.raw_x)[1:end-1]...,indices]
-        ds.raw_y = ds.raw_y[size(ds.raw_y)[1:end-1]...,indices]
+        ds.raw_x = ds.raw_x[xdims...,indices]
+        ds.raw_y = ds.raw_y[ydims...,indices]
     end
+
     a = ds.index * ds.batch_size + 1
     b = (ds.index + 1) * ds.batch_size
 
     ds.index += 1
     # FIXME use view instead of copy
     # FIXME properly slice with channel-last format
-    return ds.raw_x[:,:,:,a:b], ds.raw_y[:,a:b]
+    return ds.raw_x[xdims...,a:b], ds.raw_y[ydims...,a:b]
 end
