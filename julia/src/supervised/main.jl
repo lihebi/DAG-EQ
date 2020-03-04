1b3aaefa9041f8e3128f3f14bbdeb1efd13b7f11
Hebi Li <lihebi.com@gmail.com>
2020-03-04 23:21:55 +0800

    fix function name

--- julia/src/supervised/main.jl
+++ julia/src/supervised/main.jl
@@ -3,8 +3,8 @@ include("exp.jl")
 fc_model_fn(d) = fc_model(d=d, z=1024, nlayer=3)
 fc_dropout_model_fn(d) = fc_model(d=d, z=1024, reg=true, nlayer=3)
 
-deep_fc_model_fn(d) = fc_model_deep(d=d, z=1024, nlayer=6)
-deep_fc_dropout_model_fn(d) = fc_model_deep(d=d, z=1024, reg=true, nlayer=6)
+deep_fc_model_fn(d) = fc_model(d=d, z=1024, nlayer=6)
+deep_fc_dropout_model_fn(d) = fc_model(d=d, z=1024, reg=true, nlayer=6)
 
 eq_model_fn(d) = eq_model(d=d, z=300, reg=false, nlayer=3)
 eq_dropout_model_fn(d) = eq_model(d=d, z=300, reg=true, nlayer=3)
@@ -27,6 +27,7 @@ function main_eq()
     end
 end
 
+
 function main_fc()
     ds = [5,7,10,15,20,25,30]
     for d in ds
@@ -34,24 +35,34 @@ function main_fc()
         # exactly the same
         exp_sup(d, fc_model_fn, prefix="FC", ng=1e4, N=20, train_steps=3e5)
         # exp_sup(d, fc_dropout_model_fn, prefix="FC-reg", ng=1e4, N=20, train_steps=3e5)
-        exp_sup(d, fc_model_deep_fn, prefix="FC-deep", ng=1e4, N=20, train_steps=3e5)
+        exp_sup(d, deep_fc_model_fn, prefix="FC-deep", ng=1e4, N=20, train_steps=3e5)
         # exp_sup(d, deep_fc_dropout_model_fn, prefix="FC-deep-reg", ng=1e4, N=20, train_steps=3e5)
     end
 end
 
 function main()
-    # TODO try this
-    exp_sup(10, deep_eq_model_fn, prefix="EQ-deep-noreg", ng=1e4, N=20, train_steps=3e4)
-    exp_sup(10, deep_eq_dropout_model_fn, prefix="EQ-deep-dropout", ng=1e4, N=20, train_steps=3e4)
+    # 15/20 is already 0
+    for d in [5,10,15,20]
+        exp_sup(d, fc_model_fn, prefix="NEW-FC", ng=1e4, N=20, train_steps=3e5)
+        exp_sup(d, deep_fc_model_fn, prefix="NEW-FC-deep", ng=1e4, N=20, train_steps=3e5)
 
-    # exp_sup(10, deep_eq_model_fn, prefix="EQ-deep-noreg", ng=1e4, N=20, train_steps=3e4)
-    # exp_sup(10, eq_dropout_model_fn, prefix="EQ-dropout", ng=1e4, N=20, train_steps=3e4)
-    # exp_sup(10, eq_model_fn, prefix="EQ-noreg", ng=1e4, N=20, train_steps=3e4)
+        # exp_sup(d, fc_dropout_model_fn, prefix="FC-reg", ng=1e4, N=20, train_steps=3e5)
+        # exp_sup(d, eq_dropout_model_fn, prefix="EQ-reg", ng=1e4, N=20,
+        #         train_steps=3e4, test_throttle=20)
 
 
-    # exp_sup(30, deep_eq_dropout_model_fn, prefix="EQ-deep-dropout", ng=1e4, N=20, train_steps=3e4)
-    # exp_sup(20, deep_eq_dropout_model_fn, prefix="EQ-deep-dropout", ng=1e4, N=20, train_steps=3e4)
+        # exp_sup(d, deep_fc_dropout_model_fn, prefix="FC-deep-reg", ng=1e4, N=20,
+        #         train_steps=3e5)
+        # exp_sup(d, deep_eq_dropout_model_fn, prefix="EQ-deep-reg", ng=5e3, N=20,
+        #         train_steps=3e4, test_throttle=20)
+    end
+    for d in [5,10,15,20,25,30]
+        exp_sup(d, eq_model_fn, prefix="NEW-EQ", ng=1e4, N=20, train_steps=3e4, test_throttle=20)
+        exp_sup(d, deep_eq_model_fn, prefix="NEW-EQ-deep", ng=5e3, N=20,
+                train_steps=3e4, test_throttle=20)
+    end
 end
 
-main_fc()
-main_eq()
+# main_fc()
+# main_eq()
+main()
