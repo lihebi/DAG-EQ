6bef191e67f9ff3a39ef2b620d35afd24fd0f013
Hebi Li <lihebi.com@gmail.com>
2020-03-04 23:20:31 +0800

    save model; skip trained

--- julia/src/supervised/exp.jl
+++ julia/src/supervised/exp.jl
@@ -21,6 +21,18 @@ function exp_sup(d, model_fn; prefix="", ng=1e4, N=10, train_steps=1e5, test_thr
     expID = "$prefix-d=$d-ng=$ng-N=$N"
     @show expID
 
+
+    # detect if the expID already tested
+
+    for dir in readdir("tensorboard_logs")
+        # occursin("EQ-d=5-ng=10000-N=20", "test-EQ-d=5-ng=10000-N=20-2020-03-01T16:32:34.56")
+        if occursin(expID, dir)
+            @info "Already trained in $dir"
+            return
+        end
+    end
+
+
     ds, test_ds = gen_sup_ds_cached(ng=ng, N=N, d=d, batch_size=100)
     # ds, test_ds = gen_sup_ds_cached_diff(ng=ng, N=N, d=d, batch_size=100)
     x, y = next_batch!(test_ds) |> gpu
@@ -49,6 +61,10 @@ function exp_sup(d, model_fn; prefix="", ng=1e4, N=10, train_steps=1e5, test_thr
                print_cb=print_cb,
                test_cb=test_cb,
                train_steps=train_steps)
+
+    @info "Saving model .."
+    if !isdir("trained_model") mkdir("trained_model") end
+    @time @save "trained_model/$expID-$(now()).bson" model=cpu(model)
 
     # TODO enforcing sparsity, and increase the loss weight of 1 edges, because
     # there are much more 0s, and they can take control of loss and make 1s not
